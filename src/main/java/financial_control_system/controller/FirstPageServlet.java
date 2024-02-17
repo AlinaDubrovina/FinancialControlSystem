86a804844bc56942c11c1e71effbde19db06e3cd
Alina Dubrovina <dubrovinaa389@gmail.com>
2024-02-17 21:55:15 +0100

    feat: test commit

--- src/main/java/financial_control_system/controller/FirstPageServlet.java
+++ src/main/java/financial_control_system/controller/FirstPageServlet.java
@@ -12,6 +12,7 @@ public class FirstPageServlet extends HttpServlet {
     public FirstPageServlet() {
     }
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
